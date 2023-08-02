d6f545d3820dfe7778b12f39449711a3e6111bbb
PrinceJadav <95040464+PrinceJadav@users.noreply.github.com>
2023-08-02 13:24:16 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,22 @@
+img = "";
+  
+  
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+}
+
+function preload(){
+ img = loadImage('HQ222.jpg')
+
+}
+
+function draw(){
+  image(img, 0, 0, 640, 420);
+  fill("#FF0000");
+  nofill();
+  text("Dog, 45, 75");
+  sroke("FF0000");
+  rect(30, 60, 450, 350);
+}
+
